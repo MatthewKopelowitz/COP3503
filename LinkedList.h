887d187b68b3438ad09613959ae38eb40156bd51
MatthewKopelowitz <47190077+MatthewKopelowitz@users.noreply.github.com>
2020-10-17 17:50:24 -0400

    Finished version

--- LinkedList.h
+++ LinkedList.h
@@ -1,15 +1,21 @@
 #include <iostream>
 #pragma once
 using namespace std;
+#include <vector>
 
 template <typename T>
 class LinkedList {
+
+public:
 
     struct Node {
         //variables
         T value;
         Node* next = nullptr;
         Node* prev = nullptr;
+
+        //default constructor
+        Node() {value = {};}
 
         //constructor
         Node(T d) {value = d;}
@@ -43,7 +49,7 @@ public:
 //-----------Behaviors-------------//
     void PrintForward() const {
         Node* curr = head;
-        for (int i = 0; i < nCount; i++) {
+        for (unsigned int i = 0; i < nCount; i++) {
             cout << curr->value << endl;
             curr = curr->next;
         }
@@ -51,18 +57,26 @@ public:
 
     void PrintReverse() const {
         Node* curr = tail;
-        for (int i = nCount-1; i >= 0; i++) {
+        for (unsigned int i = nCount-1; i >= 0; i--) {
             cout << curr->value << endl;
-            curr = curr->next;
+            curr = curr->prev;
         }
     }
 
     void PrintForwardRecursive(const Node* node) const {
-
+        if (node == nullptr) {
+            return;
+        }
+        cout << node->value << endl;
+        PrintForwardRecursive(node->next);
     }
 
     void PrintReverseRecursive(const Node* node) const {
-
+        if (node == nullptr) {
+            return;
+        }
+        cout << node->value << endl;
+        PrintReverseRecursive(node->prev);
     }
 
 //------------Accessors--------------//
@@ -73,20 +87,33 @@ public:
 
     void FindAll(vector<Node*>& outData, const T& value) const {
         Node* curr = head;
-        for (int i = 0; i < nCount; i++) {
+        for (unsigned int i = 0; i < nCount; i++) {
             if (curr->value == value) {
                 Node* match = curr;
                 outData.push_back(match);
             }
+            curr = curr->next;
         }
     }
 
-    const Node* Find(const T& data) {
+    const Node* Find(const T& data) const {
         Node* curr = head;
-        for (int i = 0; i < nCount; i++) {
+        for (unsigned int i = 0; i < nCount; i++) {
             if (curr->value == data) {
                 return curr;
             }
+            curr = curr->next;
+        }
+        return nullptr;
+    }
+
+    Node* Find(const T& data) {
+        Node* curr = head;
+        for (unsigned int i = 0; i < nCount; i++) {
+            if (curr->value == data) {
+                return curr;
+            }
+            curr = curr->next;
         }
         return nullptr;
     }
@@ -96,7 +123,7 @@ public:
         if (index < 0 || index >= nCount) {
             throw out_of_range("Out of range");
         }
-        for (int i = 0; i <= index; i++) {
+        for (unsigned int i = 0; i <= index; i++) {
             if (i == index) {
                 break;
             }
@@ -110,7 +137,7 @@ public:
         if (index < 0 || index >= nCount) {
             throw out_of_range("Out of range");
         }
-        for (int i = 0; i <= index; i++) {
+        for (unsigned int i = 0; i <= index; i++) {
             if (i == index) {
                 break;
             }
@@ -138,69 +165,166 @@ public:
 //-------------Insertion-------------//
 
     void AddHead(const T& data) {
-        Node newNode = new Node;
+        Node* newNode = new Node;
         newNode->value = data;
-        newNode->next = head;
-        //head->next = newNode;
-        head = newNode;
+
+        if (nCount == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode->next = head;
+            head->prev = newNode;
+            head = newNode;
+        }
         nCount++;
     }
 
-    //This was done in Tyler's lab
     void AddTail(const T& data) {
         Node* newNode = new Node;
         newNode->value = data;
-        newNode->prev = tail;
-        tail->next = newNode;
-        tail = newNode;
+
+        if (nCount == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode->prev = tail;
+            tail->next = newNode;
+            tail = newNode;
+        }
         nCount++;
     }
 
     void AddNodesHead(const T* data, unsigned int count) {
-        Node* last = head;
-        for (int i = 0; i < count; i++) {
-            if (i != count-1) {
-
-            }
+        for (int i = (int)count-1; i >= 0; i--) {
+            AddHead(data[i]);
         }
     }
 
     void AddNodesTail(const T* data, unsigned int count) {
-
+        for (unsigned int i = 0; i < count; i++) {
+            AddTail(data[i]);
+        }
     }
 
     void InsertAfter(Node* node, const T& data) {
-
+        Node* newNode = new Node(data);
+        newNode->next = node->next;
+        newNode->prev = node;
+        node->next->prev = newNode; //
+        node->next = newNode;
+        nCount++;
     }
 
     void InsertBefore(Node* node, const T& data) {
-
+        Node* newNode = new Node(data);
+        newNode->prev = node->prev;
+        newNode->next = node;
+        node->prev->next = newNode; //
+        node->prev = newNode;
+        nCount++;
     }
 
     void InsertAt(const T& data, unsigned int index) {
-
+        if (index > nCount || index < 0) {
+            throw out_of_range("Out of range");
+        } else {
+            if (index == 0) {
+                AddHead(data);
+            } else if (index == nCount) {
+                AddTail(data);
+            } else {
+                InsertBefore(GetNode(index), data);
+            }
+        }
     }
 
 //-------------Removal--------------//
 
     bool RemoveHead() {
-
+        if (nCount == 1) {
+            delete head;
+            head = nullptr;
+            tail = nullptr;
+            nCount--;
+            return true;
+        } else if (nCount == 0) {
+            return false;
+        } else {
+            Node* nHead = head->next;
+            nHead->prev = nullptr;
+            head->prev = nullptr;
+            head->next = nullptr;
+            delete head;
+            head = nHead;
+            nCount--;
+        }
+        return true;
     }
 
     bool RemoveTail() {
-
+        if (nCount == 1) {
+            delete tail;
+            head = nullptr;
+            tail = nullptr;
+            nCount--;
+            return true;
+        } else if (nCount == 0) {
+            return false;
+        } else {
+            Node* nTail = tail->prev;
+            nTail->next = nullptr;
+            tail->next = nullptr;
+            tail->prev = nullptr;
+            delete tail;
+            tail = nTail;
+            nCount--;
+        }
+        return true;
     }
 
     unsigned int Remove(const T& data) {
-
+        unsigned int count = 0;
+        bool rmd = false;
+        do {
+            Node* curr = head;
+            rmd = false;
+            for (unsigned int i = 0; i < nCount; i++) {
+                if (curr->value == data && curr != nullptr) {
+                    curr = curr->next;
+                    RemoveAt(i);
+                    rmd = true;
+                    count++;
+                    break;
+                } else {
+                    curr = curr->next;
+                }
+            }
+        } while (rmd);
+        return count;
     }
 
     bool RemoveAt(unsigned int index) {
-
+        if (index < 0 || index > nCount-1) {
+            return false;
+        } else if (index == 0) {
+            RemoveHead();
+        } else if (index == nCount-1) {
+            RemoveTail();
+        } else {
+            Node* curr = head;
+            for (unsigned int i = 0; i < index; i++) {
+                curr = curr->next;
+            }
+            curr->prev->next = curr->next;
+            curr->next->prev = curr->prev;
+            delete curr;
+            nCount--;
+        }
+        return true;
     }
 
     void Clear() {
-
+        while (RemoveTail()) {}
     }
 
 //-------------Operators-------------//
@@ -210,7 +334,7 @@ public:
             throw out_of_range("Out of Range");
         } else {
             Node* curr = head;
-            for (int i = 0; i <= index; i++) {
+            for (int i = 1; i <= index; i++) {
                 curr = curr->next;
             }
             return curr->value;
@@ -222,7 +346,7 @@ public:
             throw out_of_range("Out of Range");
         } else {
             Node* curr = head;
-            for (int i = 0; i <= index; i++) {
+            for (int i = 1; i <= index; i++) {
                 curr = curr->next;
             }
             return curr->value;
@@ -232,7 +356,10 @@ public:
     bool operator==(const LinkedList<T>& rhs) const {
         Node* curr = head;
         Node* rhsCurr = rhs.head;
-        for (int i = 0; i < nCount; i++) {
+        if (nCount != rhs.nCount) {
+            return false;
+        }
+        for (unsigned int i = 0; i < nCount; i++) {
             if (curr->value != rhsCurr->value) {
                 return false;
             }
@@ -242,22 +369,38 @@ public:
         return true;
     }
 
-
-
 //-----------------BIG THREE-------------------//
 
     //Copy Assignment Operator
     LinkedList<T>& operator=(const LinkedList<T>& rhs) {
+        Clear();
 
+        Node* curr = rhs.head;
+        head = curr;
+        for (unsigned int i = 0; i < rhs.nCount-1; i++) {
+            AddTail(curr->value);
+            curr = curr->next;
+        }
+        AddTail(curr->value);
+        return *this;
     }
 
     //Copy Constructor
     LinkedList(const LinkedList<T>& l) {
+        Clear();
 
+        //head = l.head;
+        Node* curr = l.head;
+        for (unsigned int i = 0; i < l.nCount-1; i++) {
+            AddTail(curr->value);
+            curr = curr->next;
+        }
+        AddTail(curr->value);
     }
 
     //Destructor
     ~LinkedList() {
+        //Clear();
         Node* curr = head;
         while (curr != nullptr) {
             Node* nex = curr->next;
